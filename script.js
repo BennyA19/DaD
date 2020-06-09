b56c5f63e9e25423aaafc33d20bf4d826f94ea70
Benny <belo8392@th-wildau.de>
2020-06-09 15:20:24 +0200

    ipa Keyboard

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const clickButtonHandler = (evt) => { 
- document.querySelector('.form-control').value = evt.value;
+const clickButtonHandler = (evt) => 
+{ 
+ document.getElementById("myInput").value = document.getElementById("myInput").value
+ document.querySelector('.form-control').value = myInput.value + evt.value;
 }
