69b4f75ab19178556211e32e513c9bb7f1cfaabd
Benny <belo8392@th-wildau.de>
2021-01-19 11:12:52 +0100

    Update 2
    - Anpassung der Slug
    - Anpassung Ipa Listen Darstellung

--- dad/catalog/admin.py
+++ dad/catalog/admin.py
@@ -19,7 +19,7 @@ admin.site.register(Active)
 @admin.register(Word)
 class WORDAdmin(admin.ModelAdmin):
     list_display = ('id','entry', 'index','display_tag','updated')
-    prepopulated_fields = {'slug': ('entry',)}  #automatically populates the field with the entry value
+    prepopulated_fields = {'slug': ('entry','tag',)}  #automatically populates the field with the entry value
 
 
 #admin.site.register(ipa)
--- dad/catalog/urls.py
+++ dad/catalog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('ipas/', views.IpaListView.as_view(), name='ipas'),
     path('ipa/<int:pk>', views.IpaDetailView.as_view(), name='ipa-detail'),      ##int:pk = pk is the expected variable to continue working with (primary key)
     path('words/', views.WordListView.as_view(), name='words'),
-    path('word/<slug:slug>', views.WordDetailView.as_view(), name='word-detail'),      ##int:pk = pk is the expected variable to continue working with (primary key)
+    path('word/<slug:slug>', views.WordDetailView.as_view(), name='word-detail'),      ##slug:slug = slug is the expected variable to continue working with (primary key)
     path('audio/<int:pk>', views.index, name='audio'),
 ]
